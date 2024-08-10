e2d38f7a4b38c05820b06ed16ec7237ca18035d5
vinay.joseph <profileenrich@gmail.com>
2024-08-10 10:26:19 +0530

    first commit

---
+++ tests/GetNFTByWalletInvalidAddress.js
@@ -0,0 +1,18 @@
+import Moralis from 'moralis';
+
+try {
+  await Moralis.start({
+    apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImIxNTg0NmRlLTVjMzMtNDQ1ZS05ODYwLWVmOWEyMmU0NGQ3MSIsIm9yZ0lkIjoiNDAyOTc5IiwidXNlcklkIjoiNDE0MDg3IiwidHlwZUlkIjoiODhmOWNmYTktNmVkYS00MGJhLTgxY2ItMGFlMTdhYzc2N2U2IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MjI4NTgzMDgsImV4cCI6NDg3ODYxODMwOH0.NEMdd3kUDA3ThIC6Z6EPBeJj-77HtWr_d1DTV-qQd3E"
+  });
+
+  const response = await Moralis.EvmApi.nft.getWalletNFTs({
+    "chain": "0x1",
+    "format": "decimal",
+    "mediaItems": false,
+    "address": "xff3879b8a363aed92a6eaba8f61f1a96a9ec3c1e"
+  });
+
+  console.log(response.raw);
+} catch (e) {
+  console.error(e);
+}
---
+++ tests/getBlockByhashInvalidBlockNumber.js
@@ -0,0 +1,16 @@
+import Moralis from 'moralis';
+try {
+    await Moralis.start({
+      apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImIxNTg0NmRlLTVjMzMtNDQ1ZS05ODYwLWVmOWEyMmU0NGQ3MSIsIm9yZ0lkIjoiNDAyOTc5IiwidXNlcklkIjoiNDE0MDg3IiwidHlwZUlkIjoiODhmOWNmYTktNmVkYS00MGJhLTgxY2ItMGFlMTdhYzc2N2U2IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MjI4NTgzMDgsImV4cCI6NDg3ODYxODMwOH0.NEMdd3kUDA3ThIC6Z6EPBeJj-77HtWr_d1DTV-qQd3E"
+    });
+  
+    const response = await Moralis.EvmApi.block.getBlock({
+      "chain": "0x1",
+      "blockNumberOrHash": " "
+    });
+  
+    console.log(response.raw);
+  } catch (e) {
+    console.error(e);
+  }
+  
